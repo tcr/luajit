41f16c8ad8eaff13c1c01c957827a5ab4edf5ca1
Tim Cameron Ryan <id@timryan.org>
2014-07-15 19:33:40 -0400

    Fixes all tests to work now with -s nomenclatures.

--- dynasm/dynasm.lua
+++ dynasm/dynasm.lua
@@ -816,6 +816,7 @@ dostmt = function(stmt)
   end
 
   -- Call opcode handler or special handler for template strings.
+  _G.__op = op
   if type(f) == "string" then
     map_op[".template__"](params, f)
   else
